5a66f0a92557690b1b4435d0c024dbea318df278
LNY_fake <1598@qq.com>
2025-05-02 17:17:39 +0000

    🔧 自动更新版本号至 0.20250503011738 [skip ci]

--- draft/1.js
+++ draft/1.js
@@ -36,7 +36,7 @@ for (const file of modifiedFiles) {
   }
   
   let content = fs.readFileSync(filePath, "utf-8");
-  const newContent = content.replace(/\/\/ @version\s+.*/, `// @version     0.20250503005749
+  const newContent = content.replace(/\/\/ @version\s+.*/, `// @version     0.20250503011738
 
   if (newContent !== content) {
     fs.writeFileSync(filePath, newContent);
